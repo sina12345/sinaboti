0f1051f031915e1f6b1b6da480dc8a34d6b4ddcb
kiava <info@Kiava.ir>
2016-05-24 16:51:36 +0430

    Add files via upload

---
+++ plugins/lock_photos.lua
@@ -0,0 +1,27 @@
+kicktable = {}
+
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_photos'] then
+                lock_photos = data[tostring(msg.to.id)]['settings']['lock_photos']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_photos == "yes" then
+        delete_msg(msg.id, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+    "%[(photo)%]"
+},
+  run = run
+}
